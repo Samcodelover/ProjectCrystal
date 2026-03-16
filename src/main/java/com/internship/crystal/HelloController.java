e201d8f1833144791c7ef1388b50b7c653e58d42
Samcodelover <samal010590@gmail.com>
2026-03-16 12:07:36 -0400

    feat: initial Spring Boot setup using com.internship.crystal package

---
+++ src/main/java/com/internship/crystal/HelloController.java
@@ -0,0 +1,13 @@
+package com.internship.crystal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // This tells Java: "This class handles web requests"
+public class HelloController {
+
+    @GetMapping("/hello") // This tells Java: "If someone goes to /hello, run this"
+    public String sayHello() {
+        return "Hello World! Sam's server is officially live.";
+    }
+}
---
+++ src/test/java/com/internship/crystal/CrystalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internship.crystal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrystalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
